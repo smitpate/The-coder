ff758854a5289d938ac9bc70410d6fe36ae9e712
smitpate <87365703+smitpate@users.noreply.github.com>
2021-07-26 08:28:13 +0530

    Create device.py

---
+++ python course/device.py
@@ -0,0 +1,25 @@
+name="smit"
+sirname="patel"
+c=name+sirname
+print(c)
+print(name)
+print(sirname)
+print(c[0:9])
+print(c[-1])
+print(c[:11])
+story= "Onse upon a time there was a codder Smit Patel"
+print(len(story))
+# len will say the number of alphabates present in that code
+print(story.endswith("Patel"))
+#endswith will say true if the end word will be true and if not then false
+print(story.count("a"))
+# count will show the number of characters or word
+print(story.capitalize())
+# capitalize this function capitalize the frist character
+print(story.find("Smit"))
+# find will show the place of the word or character
+print(story.replace("there was","is"))
+# it replaces the code
+
+
+
